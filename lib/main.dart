3f45a80911c55c2dafc6d1a3caabd6bf0cb047ea
gustavomm <gustavomm22.gm@gmail.com>
2019-06-26 14:01:15 -0300

    api funcionando

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:giphy_viewer/ui/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
