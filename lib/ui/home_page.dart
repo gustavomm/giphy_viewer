511c21e5464da0a43df71e47bd1f40b6d6e00aa1
gustavomm <gustavomm22.gm@gmail.com>
2019-06-26 16:02:06 -0300

    grade de gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -13,14 +12,15 @@ class _HomePageState extends State<HomePage> {
   String _search;
   int _offset = 0;
 
-  _getGifs() async{
+  _getGifs() async {
     http.Response response;
 
-    if(_search == null){
-      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=ZigDTZDKkc8rG06GAoNo8FPHEEKfbdWs&limit=24&rating=G");
-    }
-    else {
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=ZigDTZDKkc8rG06GAoNo8FPHEEKfbdWs&q=$_search&limit=24&offset=$_offset&rating=G&lang=en");
+    if (_search == null) {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=ZigDTZDKkc8rG06GAoNo8FPHEEKfbdWs&limit=24&rating=G");
+    } else {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=ZigDTZDKkc8rG06GAoNo8FPHEEKfbdWs&q=$_search&limit=24&offset=$_offset&rating=G&lang=en");
     }
 
     return json.decode(response.body);
@@ -31,7 +31,7 @@ class _HomePageState extends State<HomePage> {
     // TODO: implement initState
     super.initState();
 
-    _getGifs().then((map){
+    _getGifs().then((map) {
       print(map);
     });
   }
@@ -39,31 +39,71 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.network(
+              "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+          centerTitle: true,
+        ),
         backgroundColor: Colors.black,
-        title: Image.network("https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.black,
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "Pesquise aqui!!",
-                labelStyle: TextStyle(color: Colors.white,),
-                border: OutlineInputBorder()
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Pesquise aqui!!",
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    border: OutlineInputBorder()),
+                style: TextStyle(color: Colors.white, fontSize: 18.0),
+                textAlign: TextAlign.center,
               ),
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0
-              ),
-              textAlign: TextAlign.center,
             ),
-          )
-        ],
-      )
+            Expanded(
+              child: FutureBuilder(
+                  future: _getGifs(),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                      case ConnectionState.none:
+                        return Container(
+                            width: 200.0,
+                            height: 200.0,
+                            alignment: Alignment.center,
+                            child: CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.white),
+                              strokeWidth: 5.0,
+                            ));
+                      default:
+                        if (snapshot.hasError)
+                          return Container();
+                        else
+                          return _createGifTable(context, snapshot);
+                    }
+                  }),
+            )
+          ],
+        ));
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+            fit: BoxFit.cover
+          ),
+        );
+      },
     );
   }
 }
